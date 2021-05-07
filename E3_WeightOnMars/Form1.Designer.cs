aa05a58a815a0eb3a47ada2a1cd90f13ba8b5627
elale1984 <edward.gratefuldead1@gmail.com>
2021-05-06 23:42:52 -0500

    Add project files.

---
+++ E3_WeightOnMars/Form1.Designer.cs
@@ -0,0 +1,166 @@
+﻿
+namespace E3_WeightOnMars
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.tb_EWeight = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tb_MWeight = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tb_EWeight
+            // 
+            this.tb_EWeight.Location = new System.Drawing.Point(287, 105);
+            this.tb_EWeight.Name = "tb_EWeight";
+            this.tb_EWeight.Size = new System.Drawing.Size(150, 31);
+            this.tb_EWeight.TabIndex = 0;
+            this.tb_EWeight.TextChanged += new System.EventHandler(this.tb_EWeight_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(134, 108);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(117, 25);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Earth Weight:";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(325, 239);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(112, 34);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Convert";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(135, 174);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(116, 25);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Mars Weight:";
+            // 
+            // tb_MWeight
+            // 
+            this.tb_MWeight.Location = new System.Drawing.Point(287, 171);
+            this.tb_MWeight.Name = "tb_MWeight";
+            this.tb_MWeight.Size = new System.Drawing.Size(150, 31);
+            this.tb_MWeight.TabIndex = 3;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(65, 95);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(63, 62);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(66, 163);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(63, 62);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 6;
+            this.pictureBox2.TabStop = false;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(110, -120);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(182, 25);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "It\'s Conversion Time!!\r\n";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("MV Boli", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label4.Location = new System.Drawing.Point(79, 29);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(348, 41);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "It\'s Conversion Time!!\r\n";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(511, 309);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.tb_MWeight);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tb_EWeight);
+            this.Name = "Form1";
+            this.Text = "Earth/Mars Weight Converter";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tb_EWeight;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tb_MWeight;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+    }
+}
+
---
+++ E3_WeightOnMars/Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+/*
+ * This application takes your weight on earth and converts it to what your weight would be on mars. 
+ * Only numbers are allowed for the input. Exception handling is in place to insure that the correct 
+ * input it received
+*/
+
+namespace E3_WeightOnMars
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            //exception handling to prevent error by incorrect user input
+            try
+            {
+                double MWeight;
+
+                //variable set to the parsed double from string
+                double EWeight = double.Parse(tb_EWeight.Text); 
+
+                if (EWeight <= 0)
+                {
+                    MessageBox.Show("Please input a weight greater than 0");
+                    tb_EWeight.Text = "";
+                   
+                    
+                }
+                else
+                {
+                    //conversion from earth weight to mars weight
+                    MWeight = EWeight * .38;
+
+                    //sets the text of the textbox to the mars weight
+                    tb_MWeight.Text = MWeight.ToString("n3");
+                }
+                
+            }
+            catch
+            {
+                //exception for user entering something other than numbers
+                MessageBox.Show("Your weight is not an algebraic expression. Enter numbers only");
+            }
+
+
+        }
+
+        //In the event of wanting to convert multiple weights, this will clear the form
+        private void tb_EWeight_TextChanged(object sender, EventArgs e)
+        {
+            tb_MWeight.Text = "";
+        }
+    }
+}
